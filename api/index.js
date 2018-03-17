ca24d78fb89a245beff7d3eed329812b9c64ddcb
MichaelFioretti <michael.fioretti@uky.edu>
2018-03-17 13:24:57 -0400

    Begin to add Google Places API

---
+++ api/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    Cities: require('./cities')
+}
--- app.js
+++ app.js
@@ -2,15 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+rp = require('request-promise');
+API = require('./api/index');
 
 var users = require('./routes/users');
 
 var app = express();
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+//define endpoints
+app.get('/api/cities', function(req, res) {
+    return API.Cities.getCities(req, res);
+});
 
 module.exports = app;
